2ebbb68486610c953cfc1acbf67e6798b83adffb
Connor Hollis <connorhollis3d@gmail.com>
2022-02-27 15:46:19 -0700

    updated service worker to cache correct files

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,16 +3,17 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
   "./index.html",
-  "./events.html",
-  "./tickets.html",
-  "./schedule.html",
-  "./assets/css/style.css",
-  "./assets/css/bootstrap.css",
-  "./assets/css/tickets.css",
-  "./dist/app.bundle.js",
-  "./dist/events.bundle.js",
-  "./dist/tickets.bundle.js",
-  "./dist/schedule.bundle.js"
+  "./css/styles.css",
+  "./js/index.js",
+  "./js/idb.js",
+  "./icons/icon-512x512.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-98x98.png",
+  "./icons/icon-72x72.png",
 ];
 
 // Respond with cached resources
